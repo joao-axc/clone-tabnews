6a2c8dcecff18518724f4b3b6c912ff116db5c9b
Joao Antonio <108196441+joao-axc@users.noreply.github.com>
2025-07-31 18:03:55 +0000

    add Prettier in project and create `lint` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 function Home() {
-    return (
-        <>
-            <h1>Página Inicial</h1>
-            <h2>Clone TabNews - Curso.dev</h2>
-        </>
-    )
+  return (
+    <>
+      <h1>Página Inicial</h1>
+      <h2>Clone TabNews - Curso.dev</h2>
+    </>
+  );
+}
+
+function testando() {
+  console.log("apenas testando prettier");
 }
 
 export default Home;
