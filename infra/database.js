6287e976be5edf4e4bebf6f3a2d327857b7c770c
Joao Cardoso <jao.ratones@gmail.com>
2025-08-06 08:43:17 -0300

    add `database.js` and `.env` file

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+const { Client } = require("pg");
+
+async function query(queryObject) {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 2 AS result;");
+  console.log(result.rows);
   response.status(200).json({
     chave: "os alunos do curso.dev são acima da média",
   });
