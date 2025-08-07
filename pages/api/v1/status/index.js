fbc5adfa9dae13240558e87469ddfafc9355bbf5
Joao Cardoso <jao.ratones@gmail.com>
2025-08-07 15:39:13 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,34 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 2 AS result;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectectionsValue =
+    databaseMaxConnectectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpennedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpennedConnectionsValue =
+    databaseOpennedConnectionsResult.rows[0].count;
+
   response.status(200).json({
-    chave: "os alunos do curso.dev são acima da média",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectectionsValue),
+        opened_connections: databaseOpennedConnectionsValue,
+      },
+    },
   });
 }
 
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,6 +1,41 @@
-describe("GET api/v1/status", () => {
-  test("should return status code '200'", async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
-    expect(response.status).toBe(200);
+describe("GET api/v1/status:", () => {
+  const response = {};
+
+  beforeAll(async () => {
+    response.head = await fetch("http://localhost:3000/api/v1/status");
+    response.body = await response.head.json();
+  });
+
+  test("Should return status code '200'", async () => {
+    expect(response.head.status).toBe(200);
+  });
+
+  describe("Related to updatedAt:", () => {
+    test("it was defined", async () => {
+      expect(response.body.updated_at).toBeDefined();
+    });
+
+    test("it is a date", async () => {
+      const parsedUpdatedAt = new Date(response.body.updated_at).toISOString();
+      expect(response.body.updated_at).toEqual(parsedUpdatedAt);
+    });
+  });
+
+  describe("Related to dependencies:", () => {
+    describe("to database:", () => {
+      test("check the version", async () => {
+        expect(response.body.dependencies.database.version).toEqual("16.0");
+      });
+      test("check the max connections", async () => {
+        expect(response.body.dependencies.database.max_connections).toEqual(
+          100
+        );
+      });
+      test("check opened connections", async () => {
+        expect(response.body.dependencies.database.opened_connections).toEqual(
+          1
+        );
+      });
+    });
   });
 });
